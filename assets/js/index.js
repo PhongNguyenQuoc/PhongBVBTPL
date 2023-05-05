046d79cea499da7e783a2b98249fa708394e6d5e
phongnq-HTV <phongnq@hybrid-technologies.vn>
2023-05-05 15:36:35 +0700

    service: done

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,4 +11,26 @@ $(document).ready(function ()
         })
     })
 
+    $(window).on('scroll', function() {
+        var headerHeight = $('.header').outerHeight(); // Lấy chiều cao của phần tử header
+        var scrollTop = $(window).scrollTop(); // Lấy vị trí scroll hiện tại của trình duyệt
+      
+        if (scrollTop > headerHeight) {
+          $('.header__nav').addClass('fixed'); // Thêm lớp fixed để cố định phần tử header
+        } else {
+          $('.header__nav').removeClass('fixed'); // Loại bỏ lớp fixed nếu không cần cố định phần tử header
+        }
+      });
+
+      $(document).ready(function() {
+        $('.can-scale').click(function() {
+          $('.overlay').addClass('active');
+          $(this).addClass('scale');
+        });
+        $('.overlay').click(function() {
+          $('.overlay').removeClass('active');
+          $('.can-scale').removeClass('scale');
+        });
+      });
+
 })
--- assets/js/service.js
+++ assets/js/service.js
@@ -2,6 +2,19 @@
 
 $(document).ready(function ()
 {
-    
+    // const Card = new Map()
+    // const KING_COMBO_TITLE = [
+    //     "1. Tư vấn kiểu tóc hợp khuôn mặt",
+    //     "2. Cắt tạo kiểu với Barber chính hiệu",
+    //     "3. Cạo mặt - cạo râu sáng da rạng ngời",
+    //     "4. Gội xả sạch bay tóc con",
+    //     "5. Vuốt sáp - xịt gôm mãi đẹp trai",
+    // ]
 
+    // const curlingHair = new Map()
+    // curlingHair.set("Uốn VIP", "")
+    // console.log(cardArr.forEach((name, path) => {
+    //     name + path
+    
+    // }));
 })
